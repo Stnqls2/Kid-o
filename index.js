0b340d5523872d6a72ce07391ccb46772c57dfe9
Stnqls2 <limser05@gmail.com>
2024-07-29 17:14:45 +0900

    2020729

--- index.js
+++ index.js
@@ -2,21 +2,61 @@ const express = require("express");
 
 const app = express();
 
+const engine = require("ejs")
+
+app.engine("html", engine.renderFile)
+app.set("veiw engine", "html")
+app.set("views", __dirname + "/views")
+
+//sql접속
+const mysql= require ('mysql2')
+const config = {
+    "host" : "localhost",
+    "port" : 3306,
+    "user" : "root",
+    "password" : "Sergo13529!@",
+    "database" : "test",
+    //"connectionlimit" : 30
+
+}
+
+const pool = mysql.createPool(config);
+
+pool.getConnection((err, conn) => {
+
+    if(!err) {
+        conn.query("insert into phone values(?, ?, ?)", 
+            ['최범규', '01022222222', 24],
+            (err, result) => {
+            console.log(result);
+            if (err) console.log(err)
+                else console.log(true)
+
+        });
+    }
+    else {
+        console.log(err)
+    }
+
+    conn.release();
+
+})
+//server port설정 
 app.set("port", 3000);
 
 //*router 설정 */
-const indexRouter = require("./routes/index");
+const indexRouter = require("./routes");
 
-const indexRou = require("./routes/jam");
+//const indexRou = require("./routes/jam");
 
 /** router 연결 */
 app.use("/index", indexRouter)
 
-app.use ("/jam", indexRou)
+
 
 app.get("/", (req, res) => {
 
-    res.send("Hello World");
+    res.render('index')
 });
 
 app.listen(app.get("port"), () => {
